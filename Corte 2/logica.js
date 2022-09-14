924d8a534698aa8d6a6a1fe449ae0e1522eacbb1
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 21:08:06 -0500

    logica

---
+++ Corte 2/logica.js
@@ -0,0 +1,9 @@
+function validar()
+{ 
+    let checkboxp = document.getElementById("p").checked;
+    console.log(checkboxp);
+
+    let checkboxq = document.getElementById("q").checked;
+    console.log(checkboxq);
+
+}
