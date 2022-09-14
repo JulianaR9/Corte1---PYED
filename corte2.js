6bde83cf7c3ae3b1dc05c35cfdce658cdd36aba9
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 19:33:04 -0500

    js

--- corte2.js
+++ corte2.js
@@ -2,15 +2,19 @@
 let nombre = "Juliana B" 
 let name = prompt ("Por favor escriba su nombre");
 let age = prompt ("Escriba su edad")
-let edadfutura = Number(age) + 10;
-let edadpasada = Number( age) -5;
+let edadfutura = Number (age) + 10;
+let edadpasada = Number (age) - 5;
+let edadtriple = Number (age) * 3;
+let edadmitad = Number (age) / 2;
 
 
 
 // alert("Hola");
 alert("Hola " + name);
-alert (name + "Su edad en 10 años será" + edadfutura)
-alert (name + "Su edad hace 5 años era" + edadpasada)
+alert (name + " su edad en 10 años será" + edadfutura)
+alert (name + " su edad hace 5 años era" + edadpasada)
+alert (name + " el triple de su edad es" + edadtriple)
+alert (name + " la mitad de su edad es" + edadmitad)
 document.write("Escuela de Artes y Letras");
 console.log ("Martes 13");
 document.getElementById("cuadro1").innerHTML= nombre;
