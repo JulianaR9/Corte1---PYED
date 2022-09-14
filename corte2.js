821a043a5f08e75a60cb4a81f21d360caadc80b2
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 19:50:29 -0500

    js

--- corte2.js
+++ corte2.js
@@ -15,7 +15,7 @@ alert (name + " su edad en 10 años será" + edadfutura)
 alert (name + " su edad hace 5 años era" + edadpasada)
 alert (name + " el triple de su edad es" + edadtriple)
 alert (name + " la mitad de su edad es" + edadmitad)
-if (age == 4 )
+/*if (age == 4 )
 {
 alert ( name + "su edad es par");
 }
@@ -23,6 +23,13 @@ else
 {
 (age ==2)
 alert ( name + " su edad es impar");
+}*/
+
+if (Number(age) >= 22){
+    alert ("su edad es mayor")
+}
+else{
+    alert("su edad es menor")
 }
 document.write("Escuela de Artes y Letras");
 console.log ("Martes 13");
