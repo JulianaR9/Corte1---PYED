bd1aa58af08c2c6bebc33d3f23d3e94fdff012d7
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 19:23:54 -0500

    js

--- corte2.js
+++ corte2.js
@@ -1,5 +1,17 @@
-alert("Hola");
-document.write("Escuela de Artes y Letras");
-console.log("Martes 13");
+
 let nombre = "Juliana B" 
+let name = prompt ("Por favor escriba su nombre");
+let age = prompt ("Escriba su edad")
+let edadfutura = Number(age) + 10;
+let edadpasada = Number( age) -5;
+
+
+
+// alert("Hola");
+alert("Hola " + name);
+alert (name + "Su edad en 10 años será" + edadfutura)
+alert (name + "Su edad hace 5 años era" + edadpasada)
+document.write("Escuela de Artes y Letras");
+console.log ("Martes 13");
 document.getElementById("cuadro1").innerHTML= nombre;
+
