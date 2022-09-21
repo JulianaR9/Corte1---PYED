361d5dc0d0c5ed032476fba6c1d151ec4e287095
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-20 19:27:47 -0500

    clase 7

--- Corte 2/clase7.js
+++ Corte 2/clase7.js
@@ -3,7 +3,7 @@ let pais =
 {
     "Nombre": "Nueva Zelanda",
     "Capital": "Wellington",
-    "Kilometros": "268.021 km",
+    "Kilometros": 268.021 ,
     "Idioma": "Inglés y Maorí",
     "PIB":"212,5 miles de millones USD"
 }
@@ -13,21 +13,75 @@ let artista =
 {
     "Nombre": "Harry Styles",
     "Nacionalidad": "Reino Unido",
-    "Genero Musical": "Pop",
-    "Canciones": "",
+    "Genero_Musical": "Pop",
+    "Canciones": 
+    [
+        {
+        "Nombre":"Late_nigth_talking",
+        "Reproducciones": "337.094.674"
+        },
+
+        {
+            "Nombre":"As_it_was",
+            "Reproducciones": "1.297.293.307"
+        },
+
+        {
+            "Nombre":"Matilda",
+            "Reproducciones": "180.488.641"
+        },
+
+    ]
+
 },
 
 {
     "Nombre": "Manuel Medrano",
     "Nacionalidad": "Colombia",
-    "Genero Musical": "Pop",
-    "Canciones": "",
+    "Genero_Musical": "Pop",
+    "Canciones":
+    [
+        {
+        "Nombre":"Bajo_el_agua",
+        "Reproducciones": "234.260,778"
+        },
+
+        {
+            "Nombre":"Afuera_del_Planeta",
+            "Reproducciones": "93.723.460"
+        },
+
+        {
+            "Nombre":"Si_Pudiera",
+            "Reproducciones": "95.738.037"
+        },
+
+    ]
 },
 
 {
     "Nombre": "Carlos Sadness",
     "Nacionalidad": "España",
-    "Genero Musical": "Pop Rock, Indie Pop",
-    "Canciones": "",
-}
+    "Genero_Musical": "Pop_Rock",
+    "Canciones":
+    [
+        {
+        "Nombre":"Sebastian_Bach",
+        "Reproducciones": ""
+        },
+
+        {
+            "Nombre":"Perseide",
+            "Reproducciones": ""
+        },
+
+        {
+            "Nombre":"Todo_Estaba_Bien",
+            "Reproducciones": "46.632.171"
+        },
+
+    ]
+},
 ]
+
+console.log(artista)
