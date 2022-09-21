5897773ccede0e97de77519890dcba4d38860e19
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-20 19:01:16 -0500

    clase 7

---
+++ Corte 2/clase7.js
@@ -0,0 +1,33 @@
+let animales = ["jirafa","delfin","mariposa","nutria","oso panda"]
+let pais = 
+{
+    "Nombre": "Nueva Zelanda",
+    "Capital": "Wellington",
+    "Kilometros": "268.021 km",
+    "Idioma": "Inglés y Maorí",
+    "PIB":"212,5 miles de millones USD"
+}
+
+let artista =
+[
+{
+    "Nombre": "Harry Styles",
+    "Nacionalidad": "Reino Unido",
+    "Genero Musical": "Pop",
+    "Canciones": "",
+},
+
+{
+    "Nombre": "Manuel Medrano",
+    "Nacionalidad": "Colombia",
+    "Genero Musical": "Pop",
+    "Canciones": "",
+},
+
+{
+    "Nombre": "Carlos Sadness",
+    "Nacionalidad": "España",
+    "Genero Musical": "Pop Rock, Indie Pop",
+    "Canciones": "",
+}
+]
