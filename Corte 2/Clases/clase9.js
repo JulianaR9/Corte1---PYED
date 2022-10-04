a257b9b6ab3b763e25d1b123f88e902bab4a3a7e
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-10-04 18:53:57 -0500

    arreglos

---
+++ Corte 2/Clases/clase9.js
@@ -0,0 +1,15 @@
+let animales = ["delfin","mariposa","jirafa","medusa","oso panda","koala","lobo","tigre","pantera","camaleon","mono","araña","zuricata","tortuga","abeja","camello","cangrejo","ardilla","calamar","pulpo"]
+
+
+
+function mostraranimales()
+{
+    let contenedoranimales = document.querySelector(#animal).value
+    for(let i=0;i<animales.length;i++)
+{
+    let animal = animales[i]; 
+    let li = document.createElement("li");
+    li.textContent = animal
+}
+
+}
