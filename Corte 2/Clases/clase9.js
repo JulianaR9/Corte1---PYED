1eec41cf7c86329d67dccdbb980cb2d63f68f4a5
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-10-04 21:11:32 -0500

    arreglo animales

--- Corte 2/Clases/clase9.js
+++ Corte 2/Clases/clase9.js
@@ -21,40 +21,85 @@ mostraranimales();
 let animales2 =
  [
     {
-        nombre:"delfin",
-        imagen:"https://estaticos.muyinteresante.es/uploads/images/gallery/5d9208eb5cafe81a0f3c986a/delfin0_0.jpg"
+        "nombre":"delfin",
+        "imagen":"https://estaticos.muyinteresante.es/uploads/images/gallery/5d9208eb5cafe81a0f3c986a/delfin0_0.jpg",
+        "peso_promedio_KG":"140",
+        "numero_patas":"0",
+        "color_predominante":"azul",
+        "alimentacion":"carnívoro" ,
+        "habitat":"océano",
     },
     {
-        nombre:"mariposa",
-        imagen:"https://www.anipedia.net/imagenes/significado-mariposas.jpg"
+        "nombre":"elefante",
+        "imagen":"https://static.nationalgeographic.es/files/styles/image_3200/public/01-wildlife-watch-elephant-hides-africa.jpg?w=1900&h=1256",
+        "peso_promedio_KG":"6000",
+        "numero_patas":"4",
+        "color_predominante":"gris",
+        "alimentacion":"herbívoro" ,
+        "habitat":"selvas y sabanas",
     },
     {
-        nombre:"camaleon",
-        imagen:"https://culturacientifica.com/app/uploads/2016/01/camaleones.jpg"
+        "nombre":"camaleon",
+        "imagen":"https://culturacientifica.com/app/uploads/2016/01/camaleones.jpg",
+        "peso_promedio_KG":"2",
+        "numero_patas":"4",
+        "color_predominante":"verde",
+        "alimentacion":"insectos" ,
+        "habitat":"bosques, selvas y llanuras",
     },
     {
-        nombre:"tortuga",
-        imagen:"https://allyouneedisbiology.files.wordpress.com/2015/07/martin_caretta_2000x13391.jpg?w=1038&h=576&crop=1"
+        "nombre":"tortuga marina",
+        "imagen":"https://allyouneedisbiology.files.wordpress.com/2015/07/martin_caretta_2000x13391.jpg?w=1038&h=576&crop=1",
+        "peso_promedio_KG":"230",
+        "numero_patas":"4",
+        "color_predominante":"café",
+        "alimentacion":"herbívoro" ,
+        "habitat":"océano",
     },
     {
-        nombre:"koala",
-        imagen:"https://www.elagoradiario.com/wp-content/uploads/2019/08/Koala-1140x600.jpg"
+        "nombre":"koala",
+        "imagen":"https://www.elagoradiario.com/wp-content/uploads/2019/08/Koala-1140x600.jpg",
+        "peso_promedio_KG":"15",
+        "numero_patas":"4",
+        "color_predominante":"gris",
+        "alimentacion":"herbívoro" ,
+        "habitat":"bosque",
     },
     {
-        nombre:"medusa",
-        imagen:"https://www.elagoradiario.com/wp-content/uploads/2021/08/medusa-1140x600.jpg"
+        "nombre":"medusa",
+        "imagen":"https://www.elagoradiario.com/wp-content/uploads/2021/08/medusa-1140x600.jpg",
+        "peso_promedio_KG":"200",
+        "numero_patas":"0",
+        "color_predominante":"azul",
+        "alimentacion":"carnívoro" ,
+        "habitat":"océano",
     },
     {
-        nombre:"pantera",
-        imagen:"https://i0.wp.com/revistadiners.com.co/wp-content/uploads/2018/09/pantera_200x800.jpg?fit=1200%2C800&ssl=1"
+        "nombre":"pantera",
+        "imagen":"https://i0.wp.com/revistadiners.com.co/wp-content/uploads/2018/09/pantera_200x800.jpg?fit=1200%2C800&ssl=1",
+        "peso_promedio_KG":"31",
+        "numero_patas":"4",
+        "color_predominante":"negro",
+        "alimentacion":"carnívoro" ,
+        "habitat":"selva",
     },
     {
-        nombre:"ardilla",
-        imagen:"https://t1.ea.ltmcdn.com/es/posts/3/6/7/donde_viven_las_ardillas_25763_orig.jpg"
+        "nombre":"ardilla",
+        "imagen":"https://t1.ea.ltmcdn.com/es/posts/3/6/7/donde_viven_las_ardillas_25763_orig.jpg",
+        "peso_promedio_KG":"1",
+        "numero_patas":"4",
+        "color_predominante":"naranja",
+        "alimentacion":"frutos secos" ,
+        "habitat":"bosques",
     },
     {
-        nombre:"jirafa",
-        imagen:"https://www.planetacurioso.com/wp-content/uploads/2017/02/JIRAFA-CURIOSIDADES-MANCHAS-compressor.jpg"
+        "nombre":"jirafa",
+        "imagen":"https://www.planetacurioso.com/wp-content/uploads/2017/02/JIRAFA-CURIOSIDADES-MANCHAS-compressor.jpg",
+        "peso_promedio_KG":"230",
+        "numero_patas":"4",
+        "color_predominante":"café",
+        "alimentacion":"herbívoro" ,
+        "habitat":"océano",
     },
     {
         nombre:"oso panda",
